1f2f3a72cec296bec80641cba147873711139b5d
user.email <stephanie.enciso13@gmail.com>
2020-12-15 17:19:57 -0600

    Finished all of MVP.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
 import React, { useState } from 'react';
 import TeamMemForm from './Form';
+import TeamMember from './TeamMember';
 
-const initialFormValues = {
+const emptyFormValues = {
   name: '',
   email: '',
   role: '',
@@ -10,12 +11,33 @@ const initialFormValues = {
 
 function App() {
   const [ teamMemList, setTeamMemList] = useState([]);
-  const [formValues, setFormValues] = useState(initialFormValues);
+  const [formValues, setFormValues] = useState(emptyFormValues);
+
+  const updateTeamMemForm = (inputName, inputValue) => {
+    setFormValues({...formValues, [inputName]: inputValue,});
+  };
+
+  const submitTeamMemForm = () => {
+    const newTeamMemList = {
+      name: formValues.name,
+      email: formValues.email,
+      role: formValues.role,
+    };
+
+    if (!newTeamMemList.name || !newTeamMemList.email || !newTeamMemList.role) return;
+
+    setTeamMemList([newTeamMemList, ...teamMemList]);
+    setFormValues(emptyFormValues);
+  }
 
 
   return (
     <div className="App">
-      <TeamMemForm values = {formValues}/>
+      <TeamMemForm values = {formValues} update = {updateTeamMemForm} submit = {submitTeamMemForm}/>
+
+      {teamMemList.map((teamMem) => {
+        return <TeamMember key = {teamMem.id} info = {teamMem} />
+      })}
     </div>
   );
 }
--- src/Form.js
+++ src/Form.js
@@ -3,16 +3,27 @@ import React from 'react';
 function TeamMemForm(props) {
     const { values, update, submit } = props;
 
+    const change = (event) => {
+        const { name, value } = event.target;
+        update(name, value);
+    };
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        submit();
+    };
+
     return (
-        <form>
-            <div className='form-inputs'>
+        <form onSubmit = {onSubmit}>
+            <div className='form-input'>
                 <label>
                     Name  
                      <input name ='name'
                       type = 'text' 
                       placeholder = 'Name'
                       maxLength = '30' 
-                      value = {values.name} />
+                      value = {values.name}
+                      onChange = {change} />
                 </label>
 
                 <label>
@@ -21,24 +32,25 @@ function TeamMemForm(props) {
                       type = 'text' 
                       placeholder = 'Email'
                       maxLength = '40' 
-                      value = {values.email} />
+                      value = {values.email}
+                      onChange = {change} />
                 </label>
 
                 <label>
                     Role 
-                     <select name = 'role' value = {values.role}>
+                     <select name = 'role' value = {values.role} onChange = {change}>
                         <option value = ''>----Choose Option----</option>
-                        <option value = 'backend'>Backend Engineer</option>
-                        <option value = 'frontend'>Frontend Engineer</option>
-                        <option value = 'designer'>Designer</option>
-                        <option value = 'seniorWD'>Senior Web Developer</option>
-                        <option value = 'juniorWD'>Junior Web Developer</option>
-                        <option value = 'data-analyst'>Data Analyst</option>
+                        <option value = 'Backend Engineer'>Backend Engineer</option>
+                        <option value = 'Frontend Engineer'>Frontend Engineer</option>
+                        <option value = 'Designer'>Designer</option>
+                        <option value = 'Senior Web Developer'>Senior Web Developer</option>
+                        <option value = 'Junior Web Developer'>Junior Web Developer</option>
+                        <option value = 'Data Analyst'>Data Analyst</option>
                      </select>
                 </label>
-                
+
                 <div>
-                    <button className = 'submit'>Submit</button>
+                    <button>Submit</button>
                 </div>
             </div>
         </form>
